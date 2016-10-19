3eaf057f75f0a5af0eac10e12b5c6cff96ac4433
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-10-20 00:00:30 +0200

    fix lint

--- homeassistant/components/camera/generic.py
+++ homeassistant/components/camera/generic.py
@@ -5,7 +5,6 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/camera.generic/
 """
 import asyncio
-import functools as ft
 import logging
 
 import aiohttp
